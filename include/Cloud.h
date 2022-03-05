0871963dcad9824351b7e89e5b2213dff0ca761c
ChenRP07 <1046230423@qq.com>
2022-03-05 13:17:52 +0800

    cloud

---
+++ include/Cloud.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Chen Ruopeng, SDU, Univ.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Mozilla Public license (found in the
+ * LICENSE file in the root directory of this source tree).
+ */
+
+class Cloud
+{
+    
+}
---
+++ include/registration.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Chen Ruopeng, SDU, Univ.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Mozilla Public license (found in the
+ * LICENSE file in the root directory of this source tree).
+ */
+
+#include <pcl/registration/icp.h>
