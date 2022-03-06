2b986152a4f4bf4201260d486029f46ade6bd9cb
ChenRP07 <1046230423@qq.com>
2022-03-06 15:46:59 +0800

    cloud.set_point_cloud

--- include/Cloud.h
+++ include/Cloud.h
@@ -5,8 +5,21 @@
  * This source code is licensed under the Mozilla Public license (found in the
  * LICENSE file in the root directory of this source tree).
  */
+#include <string>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <eigen3/Eigen/Dense>
 
 class Cloud
 {
+public:
+    pcl::PointCloud<pcl::PointXYZRGB> point_cloud;
+    pcl::PointCloud<pcl::PointXYZRGB> ref_point_cloud;
+    Eigen::Matrix4f translation_matrix;
+
+    Cloud();
+    void set_point_cloud(std::string);
+    void set_ref_point_cloud(std::string);
+
     
 }
